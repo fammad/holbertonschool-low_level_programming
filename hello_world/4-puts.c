b51c8869107d5cb0d51388cd509b65ca36670db5
fammad <fred.mmov@gmail.com>
2023-10-15 09:00:37 -0700

    aded4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-/*aded descritpion for betty
- *OMG
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message
+ * to the console.
+ *
+ * Return: 0 on success
  */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
