57cf9629a7093c55839da5d8ced1cedc5efdad9a
fammad <fred.mmov@gmail.com>
2023-10-17 00:08:01 -0700

    fixed

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Desc:Something
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-	putchar (alphabet);
+	putchar(alphabet);
 	}
-	putchar ('\n');
-	return 0;
+	putchar('\n');
+	return (0);
 }
