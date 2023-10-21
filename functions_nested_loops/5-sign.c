5258210333748c5f551d54fbe9f43cfa084bbc2f
fammad <fred.mmov@gmail.com>
2023-10-21 01:22:04 -0700

    1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_sign - a function checking the value is poitive or negative
+ *@n : is variable
+ *Description : A prgram that copare values
+ *Return : 0, 1, or -1 with symbols
+ *
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		printf ("-");
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf ("+");
+		return (1);
+	}
+}
