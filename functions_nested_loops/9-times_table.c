52ad0fcf342390048cd11d6db4bf2daa9fc8dc11
fammad <fred.mmov@gmail.com>
2023-10-21 06:56:20 -0700

    fixed

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,32 +9,34 @@
  */
 void times_table(void)
 {
-	int cycle1;
-	int cycle2;
-	int value;
+    int cycle1;
+    int cycle2;
+    int value;
 
-	for (cycle1 = 0; cycle1 <= 9; cycle1++)
-	{
-		for (cycle2 = 0; cycle2 <= 9; cycle2++)
-		{
-			value = cycle1 * cycle2;
+    for (cycle1 = 0; cycle1 <= 9; cycle1++)
+    {
+        for (cycle2 = 0; cycle2 <= 9; cycle2++)
+        {
+            value = cycle1 * cycle2;
 
-			if (value >= 10 && cycle2 != 9)
-			  {
-				printf("%d, ", value);
-			  }
-			else if ((value >= 10 && cycle2 == 9))
+            if (value >= 10)
+            {
+                printf("%d", value);
+            }
+            else
+            {
+                printf(" ");
+                printf("%d", value);
+            }
+
+            if (cycle2 < 9)
 			{
-				printf("%d\n", value);
-			}
-			else if (value < 10 && cycle2 != 9)
-			  {
-				printf("%d,  ", value);
-			  }
-			else
-              {
-                printf("%d\n", value);
-              }
-		}
-	}
+                printf(", ");
+            }
+            else
+            {
+                printf("\n");
+            }
+        }
+    }
 }
