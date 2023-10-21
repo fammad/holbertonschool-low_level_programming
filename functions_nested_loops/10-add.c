4cf855d30bce7d09dd3d0cbf69da244671b0a730
fammad <fred.mmov@gmail.com>
2023-10-21 12:04:15 -0700

    added

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - adds the tables
+ *a - is one number
+ *b - is another muber
+ *sum - is the result
+ */
+
+int add(int, int)
+{
+	int a, b, sum;
+
+	sum = a + b;
+
+	return (sum);
+}
