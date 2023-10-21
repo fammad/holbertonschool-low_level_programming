b92776dd2c9fc467dcff592e6f32d3f209353c0b
fammad <fred.mmov@gmail.com>
2023-10-21 00:24:15 -0700

    aded

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,14 +4,16 @@
  * main - Entry point
  *
  * Desc:Something
+ * 
+ * print_alphabet - THis funcion explain description of that function
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-  char count;
-  
-  for (count = 'a'; count <= 'z'; count++)
+	char count;
+
+	for (count = 'a'; count <= 'z'; count++)
 	{
 		putchar(count);
 	}
