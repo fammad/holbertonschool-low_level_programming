6435081671b2ba4c595b5a1226146af1e38b9fad
fammad <fred.mmov@gmail.com>
2023-10-21 12:24:40 -0700

    added11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - adds the tables
+ *@a: variable
+ *@b: variable
+ *Return: sum of variables
+ */
+
+void print_to_98(int n)
+{
+  for (n; n <= 98; n++)
+	{
+	  printf("%d, ", n);
+	}
+	}
