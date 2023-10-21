266fd16bd1e3698d22f850ee9abdab75c3a89371
fammad <fred.mmov@gmail.com>
2023-10-21 13:01:49 -0700

    editednspace

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -23,20 +23,20 @@ void print_to_98(int n)
 		}
 	}
 	else if (n > 98)
-    {
+		{
 		while (n >= 98)
-    	{
-    		printf("%d", n);
+			{
+			printf("%d", n);
 			if (n != 98) 
 			{
 				printf(", ");
 			}
 			n--;
- 		}
+			}
 	}
 	else if (n == 98)
 	{
-     	printf("%d", n);
-    }
+		printf("%d", n);
+	}
 	putchar('\n');
 }
