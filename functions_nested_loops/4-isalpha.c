f8f7ef5ed6de0c52e193f4fd538c9e1f4298c6a7
fammad <fred.mmov@gmail.com>
2023-10-21 01:06:21 -0700

    1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_isalpha - Check if string is lowercase or uppecrcase
+*@c: variable
+*Description: Up or lowercase tast
+*Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
