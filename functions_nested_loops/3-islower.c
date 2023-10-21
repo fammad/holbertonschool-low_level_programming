17f99bd5d38b847d29d87de0958593b52581cc36
fammad <fred.mmov@gmail.com>
2023-10-21 00:55:54 -0700

    1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,17 +2,17 @@
 #include "main.h"
 /**
  *_islower -checks for lowercases
- * c value is interger that we checking
- * returnv vaule is the result that if the value is lowercase or not
+ *@c value is interger that we checking
+ *Return vaule is the result that if the value is lowercase or not
 */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
