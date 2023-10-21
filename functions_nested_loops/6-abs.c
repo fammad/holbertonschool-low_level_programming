5f80a213d780df5d9741121c96aa00a3371ab52d
fammad <fred.mmov@gmail.com>
2023-10-21 02:05:00 -0700

    nsoald

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_abs - a function checking the value is +- and making it +
- *@var : is variable
+ *_abs - making absolute number 
+ *@var: is positive or negative nuber 
  *Description : A prgram that makes absolute value
- *Return: 0
+ *Return: val
 */
 int _abs(int val)
 {
