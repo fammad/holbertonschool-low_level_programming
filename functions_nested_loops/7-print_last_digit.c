9a4d41091045d50407ef0912dc0bed3720443f8d
fammad <fred.mmov@gmail.com>
2023-10-21 04:13:49 -0700

    0

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - Check if string is lowercase
+*@v: the integer to be converted
+*
+*Description: Program of task 6
+*Return: absolute value of the input
+*/
+int print_last_digit(int v)
+{
+	v = v%10;
+	return (v);
+}
