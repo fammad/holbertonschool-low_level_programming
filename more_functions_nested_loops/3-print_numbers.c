911250e24849e686cd97019d655fac08fad29796
fammad <fred.mmov@gmail.com>
2023-10-22 08:07:12 -0700

    askdnas

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - function checks if number is between 0-9
+ *@a: variable
+ *@b: variable
+ *Return: Sum of the file
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	  {
+		_putchar(n + '0');
+	  }
+	_putchar('\n');
+}
