1871d6e67e2ef04ed82a969d8d608bb2fa9a3ab7
fammad <fred.mmov@gmail.com>
2023-10-22 12:00:44 -0700

    aded

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,18 +14,16 @@ void more_numbers(void)
 	for (t = 0; t <= 9; t++)
 	{
 		for (n = 0; n <= 14; n++)
-		{
+		  {
 			if (n >= 10)
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
 			}
 			else
 			{
 				_putchar(n + '0');
 			}
 		}
-		printf("\n");
+		_putchar('\n');
 	}
-	printf("\n");
 }
