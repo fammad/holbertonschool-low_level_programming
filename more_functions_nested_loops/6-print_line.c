af1d99a0bf29bf9218c24fcaee437ede6e8e2e79
fammad <fred.mmov@gmail.com>
2023-10-22 12:34:52 -0700

    fixed

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,9 @@ void print_line(int n)
 {
 	int b;
 
-	if (n <= 0)
+	for (b = 0; b < n; b++)
 	{
-		 _putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (b = 0; b < n; b++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
