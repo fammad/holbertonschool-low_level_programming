73e8b75406ce894f4a7f0aff5bab231bb28c0a2c
fammad <fred.mmov@gmail.com>
2023-10-22 12:14:31 -0700

    aded

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_line - function doing what it says
+ *@n : variable
+ *
+ *
+ *
+ */
+void print_line(int n)
+{
+	int b;
+	for (b = 0; b <= n; b++)
+	{
+		if(n <= 0)
+		{
+			_putchar('\n');	
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+}
