01f746397e7d35510942b733991fe3f243512f1d
fammad <fred.mmov@gmail.com>
2023-10-22 05:57:39 -0700

    added

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - function checks if number is between 0-9
+ *@a: variable
+ *@b: variable
+ *Return: Sum of the file
+ *
+ */
+int mul(int a, int b)
+{
+  int mul = a * b;
+  return (mul);
+}
