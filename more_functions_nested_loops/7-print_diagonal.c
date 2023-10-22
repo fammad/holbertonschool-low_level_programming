9a2da2ec860cc42410d9ec5eb93f6751fcaafcb3
fammad <fred.mmov@gmail.com>
2023-10-22 12:43:23 -0700

    added

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagonal - function doing what it says
+ *@n : variable
+ *
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+}
