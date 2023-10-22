bc36ceba0d0e7cad81be1ead501b9d5b4d103d03
fammad <fred.mmov@gmail.com>
2023-10-22 08:11:02 -0700

    fixed

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *mul - function checks if number is between 0-9
- *@a: variable
- *@b: variable
- *Return: Sum of the file
+ *print_numbers - function doing what it says
+ *
+ *
+ *
  *
  */
 void print_numbers(void)
 {
 	int n;
+
 	for (n = 0; n <= 9; n++)
-	  {
+	{
 		_putchar(n + '0');
-	  }
+	}
 	_putchar('\n');
 }
