1aa99d913fe2fed17c1117fcea812f2c2c878839
fammad <fred.mmov@gmail.com>
2023-10-22 11:26:52 -0700

    added

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_most_numbers - function doing what it says
+ *
+ *
+ *
+ *
+ */
+void print_most_numbers(void);
+{
+	int n = 0;
+
+	while (n <=9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
