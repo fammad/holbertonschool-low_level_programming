14ce0429779b3a948befd8cb2e4abe1ebe8ccbc5
fammad <fred.mmov@gmail.com>
2023-10-24 08:18:25 -0700

    aded101

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -4,13 +4,15 @@
  *@n : variable
  *
 */
-void print_number(int n) {
+void print_number(int n)
+{
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0) {
+	if (n / 10 != 0)
+	{
 		print_number(n / 10);
 	}
 	_putchar(n % 10 + '0');
