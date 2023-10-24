2e88aed2231e4fcde10bbe8eae7bb5509f80aef5
fammad <fred.mmov@gmail.com>
2023-10-24 07:55:26 -0700

    I

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_triangle - function doing what it says
+ *@size : variable
+ *
+ *
+ *
+ */
+void print_triangle(int size)
+{
+	int hash;
+	int row;
+	int space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row == 1; row <= size; row++)
+		{
+			for (space == 1; space <= size - row; space++)
+			{
+				_putchar(' ');
+			}
+			for (hash == 1; hash <= row; hash++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
