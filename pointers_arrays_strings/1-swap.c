42adcae63a7a808d5eb7d4fe8194b507bd961ce6
fammad <fred.mmov@gmail.com>
2023-10-25 11:32:02 -0700

    added

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*swap_int - does what it say
+*@a: Variable
+*@b: Variable
+*/
+void swap_int(int *a, int *b)
+{
+	int holder;
+  
+	holder = *a 
+	*b = *a;
+	*a = holder;
+}
