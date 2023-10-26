1dbab271c696f54c3142ec5ef02a9fcedb358ae9
fammad <fred.mmov@gmail.com>
2023-10-26 00:25:43 -0700

    adeed

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+*print_rev - does what it say
+*@s: Variable
+*Return:Lenght of interger
+*/
+void print_rev(char *s)
+{
+  	int lenght, cycle;
+
+	lenght = strlen(s);
+	for (cycle = 0; cycle < lenght; cycle++)
+	{
+		cor = lenght - cycle);
+		puts(*(s - cor); 
+	}
+}
