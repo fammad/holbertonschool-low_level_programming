b5db670222d84b127375b6447581966213742ec5
fammad <fred.mmov@gmail.com>
2023-10-26 11:44:18 -0700

    added_10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ *_strcpy - is a fucntion that doimg something
+ *@s: Variable
+ *
+ *Return: Interger
+*/
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
