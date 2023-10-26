cb2788c27738c0e536d82a41415bf604b3eb8174
fammad <fred.mmov@gmail.com>
2023-10-25 23:19:47 -0700

    aded

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strlen - does what it say
+*@s: Variable
+*Return:Lenght of interger
+*/
+int _strlen(char *s)
+{
+  int lenght;
+
+  lenght = sizeof(s) - 1;
+  return (lenght);
+}
