a6198f1b5910ebfc92102e069316c576efa5bcd0
fammad <fred.mmov@gmail.com>
2023-10-26 05:51:05 -0700

    added_6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+*puts2 - does what it say
+*@str: Variable
+*Return:Lenght of interger
+*/
+void puts2(char *str)
+{
+	int cy;
+	int len = strlen(str);
+  
+	for (cy = 0; cy < len; cy++)
+	{
+		if (cy % 2 == 0)
+		{
+			puts(cy);
+		}
+	}
+}
