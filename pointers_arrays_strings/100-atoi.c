bf888dd2434fd490a4e37bcef8da61792e920a48
fammad <fred.mmov@gmail.com>
2023-10-27 06:19:11 -0700

    fixed_100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,34 +8,28 @@
  *
  *Return: Interger
 */
-int _atoi(char *s)
-{
-	int result = 0;
-	int sign = 1;
+int _atoi(char *s) {
+    int result = 0;
+    int sign = 1;
 
-	while (*s == ' ')
-		s++;
+    while (*s) {
+        if (*s == '-') {
+            sign = -1;
+        } else if (*s >= '0' && *s <= '9') {
+            int digit = *s - '0';
+            if (result > (2147483647 - digit) / 10) {
+                if (sign == 1) {
+                    return 2147483647;
+                } else {
+                    return -2147483648;
+                }
+            }
+            result = result * 10 + digit;
+        } else if (result != 0) {
+            break;
+        }
+        s++;
+    }
 
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
-	else if (*s == '+')
-	{
-		s++;
-	}
-	while (*s >= '0' && *s <= '9')
-	{
-		if (result > (2147483647 - (*s - '0')) / 10)
-		{
-			if (sign == 1)
-				return 2147483647;
-			else
-				return -2147483648;
-		}
-		result = result * 10 + (*s - '0');
-		s++;
-	}
-	return result * sign;
+    return result * sign;
 }
