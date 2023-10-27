de6daec2e9b6b93f205b49c46db20157bcabca3e
fammad <fred.mmov@gmail.com>
2023-10-27 06:26:38 -0700

    fixed_100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,33 +3,45 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- *_strcpy - is a fucntion that doimg something
+ *_atoi - is a fucntion that doimg something
  *@s: Variable
  *
  *Return: Interger
 */
-int _atoi(char *s) {
-    int result = 0;
-    int sign = 1;
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
 
-    while (*s) {
-        if (*s == '-') {
-            sign = -1;
-        } else if (*s >= '0' && *s <= '9') {
-            int digit = *s - '0';
-            if (result > (2147483647 - digit) / 10) {
-                if (sign == 1) {
-                    return 2147483647;
-                } else {
-                    return -2147483648;
-                }
-            }
-            result = result * 10 + digit;
-        } else if (result != 0) {
-            break;
-        }
-        s++;
-    }
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			int digit = *s - '0';
 
-    return result * sign;
+			if (result > (2147483647 - digit) / 10)
+			{
+                if (sign == 1)
+				{
+					return 2147483647;
+				}
+				else
+				{
+					return -2147483648;
+				}
+			}
+			result = result * 10 + digit;
+		}
+		else if (result != 0)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return result * sign;
 }
