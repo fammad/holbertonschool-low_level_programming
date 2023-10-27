5f6af9bde840173a2ef1aa5b14769a7b29f928fb
fammad <fred.mmov@gmail.com>
2023-10-27 05:53:49 -0700

    fixed_100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,28 +10,22 @@
 */
 int _atoi(char *s)
 {
-	while (*s == ' ')
-	{
- 		s++;
-	}
+    int sign = 1;
+    int result = 0;
 
-	int sign = 1;
+    while (*s == ' ')
+        s++;
 
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == '-')
-		{
-    		sign = -1;
-    	}
-    	s++;
-	}
+    if (*s == '-' || *s == '+') {
+        if (*s == '-')
+            sign = -1;
+        s++;
+    }
 
-	int result = 0;
+    while (*s >= '0' && *s <= '9') {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
 
-	while (*s >= '0' && *s <= '9')
-	{
-	result = result * 10 + (*s - '0');
-	s++;
-	}
-	return result * sign;
+    return result * sign;
 }
