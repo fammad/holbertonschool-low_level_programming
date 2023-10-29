8d16be2c338b9dac1754b0505afbd3208ca41362
fammad <fred.mmov@gmail.com>
2023-10-29 02:01:35 -0700

    added

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strcat: is concatenates two strings
+ *@dest: Variable
+ *@src: Variable
+ *Return: Char
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;  
+	}
+	*dest = '\0';
+	return (result);
+}
