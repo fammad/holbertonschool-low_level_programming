8c1faf05123bacd2665ff7114129b210a037cf68
fammad <fred.mmov@gmail.com>
2023-10-29 12:08:30 -0700

    added_6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*cap_string - Making everything uppercase
+*@str: Variable
+*A program that src string and dest string doing something.
+*Return: Void
+*/
+char *cap_string(char *str)
+{
+	int capitalize = 1;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (isspace(str[i]) || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			capitalize = 1;
+		}
+		else if (isalpha(str[i]))
+		{
+			if (capitalize)
+			{
+				str[i] = toupper(str[i]);
+				capitalize = 0;
+			}
+			else
+			{
+				str[i] = tolower(str[i]);
+			}
+		}
+	}
+	return (a);
+}
