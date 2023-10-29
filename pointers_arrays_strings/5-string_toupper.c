ecf3299da9d1c411d187749c1583d0ece53e5b7f
fammad <fred.mmov@gmail.com>
2023-10-29 07:59:45 -0700

    added_5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*reverse_array - Comparing one to the another
+*@a: Variable
+*A program that src string and dest string doing something.
+*Return: Void
+*/
+char *string_toupper(char *a)
+{
+	int start;
+	int end;
+
+	for(start = 0; a[start] != '\0'; start++)
+	{
+		if (a[start] >= 'a' && a[start] <= 'z')
+		{
+			a[start] = a[start] - ('a' - 'A');
+		}			
+	}
+	return (a);
+}
