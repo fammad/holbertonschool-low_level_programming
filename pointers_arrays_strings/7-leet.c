62b2008d5097987a4b5cc405bee380f15cee3a5a
fammad <fred.mmov@gmail.com>
2023-10-29 12:46:55 -0700

    edited_7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - Encodes a string .
+ * @str: Variable
+ *
+ * Return: A pointer
+ */
+char *leet(char *str)
+{
+	char *replacement = "aAeEoOtTlL";
+	char *encoded = "44337711";
+
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; replacement[j] != '\0'; j++)
+		{
+			if (str[i] == replacement[j])
+			{
+				str[i] = encoded[j];
+				break;
+			}
+		}
+	}
+	return str;
+}
