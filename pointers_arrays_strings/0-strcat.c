98f4c39871eaaede562a0ad2eb1f09c1a2b5006e
fammad <fred.mmov@gmail.com>
2023-10-29 02:10:11 -0700

    edited_0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *_strcat: is concatenates two strings
- *Description: A function doing something 
- *@dest: Variable
- *@src: Variable
- *Return: Char
+*_strcat: is concatenates two strings
+*@dest: Variable
+*@src: Variable
+*A program that src string and dest string doing something.
+*Return: Char
 */
 char *_strcat(char *dest, char *src)
 {
