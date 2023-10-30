8c55219e5ee900964392bf66c66597faca3d25af
fammad <fred.mmov@gmail.com>
2023-10-30 01:01:56 -0700

    added_0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_memset is a fucntion that fills the space
+ *@s Variable
+ *@b Variable
+ *@n Variable
+ *Return: A char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		s[index] = b;
+	}
+	return (s);
+}
