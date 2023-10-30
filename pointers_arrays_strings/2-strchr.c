ff998ea38edfb05e3a3151c2c116c1851e69c695
fammad <fred.mmov@gmail.com>
2023-10-30 05:44:26 -0700

    added_2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strchr - function that locates a character in a string
+*@s: Variable
+*@c: Variable
+*A program that src string and dest string doing something.
+*Return: Char
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+}
