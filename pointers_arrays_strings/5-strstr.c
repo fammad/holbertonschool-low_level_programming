f3bf7d9d75b857ec4875df56cbdce75589989849
fammad <fred.mmov@gmail.com>
2023-10-31 01:05:31 -0700

    added_5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,9 +2,9 @@
 #include "main.h"
 #include <string.h>
 /**
-*_find_substring - function that searches a string for any of a set of bytes
-*@text: Variable
-*@substring: Variable
+*_strstr - function that searches a string for any of a set of bytes
+*@haystack: Variable
+*@needle: Variable
 *A program that src string and dest string doing something.
 *Return: Char
 */
@@ -13,18 +13,18 @@ char *_strstr(char *haystack, char *needle)
 	size_t text_len = strlen(haystack);
 	size_t sub_len = strlen(needle);
 	size_t i;
-	
+
 	if (sub_len == 0)
 	{
 		return (haystack);
-    }
-    if (text_len < sub_len)
+	}
+	if (text_len < sub_len)
 	{
 		return (NULL);
 	}
 	for (i = 0; i <= text_len - sub_len; i++)
 	{
-		if (strncmp(haystack + i,needle, sub_len) == 0)
+		if (strncmp(haystack + i, needle, sub_len) == 0)
 		{
 			return (haystack + i);
 		}
