88d371c180b7b7a90a2aceb3ac67c9eb6e8fe364
fammad <fred.mmov@gmail.com>
2023-11-03 23:41:54 -0700

    added_1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ *_print_rev_recursion - function that prints a string in reverse
+ *@s: Variable
+ *Return: Void
+ *
+*/
+void _print_rev_recursion(char *s)
+{
+	strrev(s);
+}
