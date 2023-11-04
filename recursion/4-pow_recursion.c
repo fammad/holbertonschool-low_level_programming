905407a65348e67081247985bec00df8c62b4118
fammad <fred.mmov@gmail.com>
2023-11-04 01:21:36 -0700

    added_4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_pow_recursion - function find to return  x power of y
+ *@x: Variable
+ *@y: Variable
+ *Return: Void
+ *
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+  	else if (y == 0)
+		return (1);
+	else
+		return (x * return (x, y-1);
+  
+}
