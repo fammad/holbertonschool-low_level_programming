da59d60137760336edebfb9089be7afd8a9e3659
fammad <fred.mmov@gmail.com>
2023-11-04 02:29:19 -0700

    aded_5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,19 +3,21 @@
 #include <string.h>
 /**
  *_sqrt_recursion - function that returns the square root of n
+ *_guess_squareroot - function that rying to guess 
  *@n: Variable
+ *@guess: Variable
  *Return: Squareroot of n
  *
 */
 int _sqrt_recursion(int n)
 {
-  return (_guess_squareroot(n, 1));
+	return (_guess_squareroot(n, 1));
 }
 
 int _guess_squareroot(int n, int guess)
 {
 	if (guess * guess == n)
-  		return (guess);
+		return (guess);
 	else if (guess * guess > n)
 		return (-1);
 	return (_guess_squareroot(n, guess + 1));
