9d41664954a238e30b6b89d3ca3ff4be5acb1b96
fammad <fred.mmov@gmail.com>
2023-11-18 11:35:59 -0800

    added_2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	int len1;
 	int len2;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	len1 = strlen(s1);
 	len2 = strlen(s2);
