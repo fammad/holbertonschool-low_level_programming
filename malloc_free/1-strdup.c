f3d48af385dc4503d1d19f179cabf04219bd38b2
fammad <fred.mmov@gmail.com>
2023-11-18 11:07:49 -0800

    added_1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,10 +11,12 @@ char *_strdup(char *str)
 {
 	char *arr;
 	int i;
-
+	int len;
+	
 	if (str == NULL)
 		return (NULL);
-	arr = (char *)malloc(strlen(str) * sizeof(char) + 1);
+	len = strlen (str);
+	arr = (char *)malloc ((len) * sizeof(char) + 1);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
