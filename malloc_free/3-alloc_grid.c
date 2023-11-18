c00998806a33258e061d4cd8c3775b852a939aaf
fammad <fred.mmov@gmail.com>
2023-11-18 13:45:55 -0800

    added_3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
 int **alloc_grid(int width, int height)
 {
   int **ret;
-  int i1, i2;
+  int i1, i2, iclean;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -19,6 +19,14 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i1 = 0; i1 < height; i1++)
 	{
+		if (ret == NULL)
+			for (iclean = 0; iclean < i1; iclean++)
+			{
+				free(ret[iclean]);
+			}
+			free(ret);
+			return (NULL);
+		
 		ret[i1] = (int *)malloc(width * sizeof(int));
 		for (i2 = 0; i2 < width; i2++)
 		{
