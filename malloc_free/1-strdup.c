35fdb0604e4c18e305d82ffff55734ffb92fc6b1
fammad <fred.mmov@gmail.com>
2023-11-18 10:55:16 -0800

    added_1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  *_strdup - function that returns a pointer to a newly allocated space in memory
  *@str: Variable for string
