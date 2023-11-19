e1ffc2ad1a48d8b486eef66b0948762011e7b128
fammad <fred.mmov@gmail.com>
2023-11-18 22:35:35 -0800

    added_4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *free_grid - function that frees a 2 dimensional grid 
+ *free_grid - function that frees a 2 dimensional grid
  *@grid: variable double pointer
  *@height: variable integer
  *Return: nothing
  */
 void free_grid(int **grid, int height)
 {
-  int i;
-  
+	int i;
+
 	if (grid == NULL || height <= 0)
 		return;
 	for (i = 0; i < height; i++)
 	{
-	  free(grid[i]);
+		free(grid[i]);
 	}
 	free(grid);
 }
