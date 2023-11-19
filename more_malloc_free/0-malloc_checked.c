efb4a302a56c6b0ec27eeca85127dc547578b972
fammad <fred.mmov@gmail.com>
2023-11-19 11:06:41 -0800

    added

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * malloc_checked - Function that allocates memory using malloc.
- * @b: Variable
- *
- * Return: A pointer to the allocated memory.
- * 
- */
+* malloc_checked - Function that allocates memory using malloc.
+* @b: Variable
+* Return: A pointer to the allocated memory
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
