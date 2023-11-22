1c16f12c363531f175e88f247e00748021c34e3c
fammad <fred.mmov@gmail.com>
2023-11-22 13:34:35 -0800

    added_0

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,7 +13,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		exit(EXIT_FAILURE);
+		printf("Ok\n");
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
