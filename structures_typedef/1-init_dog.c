550f1908b04d5d7469a58d435f119520307c7b20
fammad <fred.mmov@gmail.com>
2023-11-22 13:18:38 -0800

    added_1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include
+
+/**
+ *init_dog - Funciton that initialize the values
+ *struct dog - Structure initialization
+ *@d: Pointer Variable
+ *@name: Pointer Variable
+ *@age: Floatin Variable
+ *@owner: Pointer Variable
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+  d->name = name;
+  d->age = age;
+  d->owner = owner;
+}
