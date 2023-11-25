084b9947e827bbc2d39c53c82cde58c10fff947b
fammad <fred.mmov@gmail.com>
2023-11-25 12:21:13 -0800

    added_2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	printf("Age: %.f\n", d->age);
 	if (d->owner != NULL)
-		printf("Owner: %.f\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 	else
 	printf("Owner: (nil)\n");
 }
