fbf67954d3a5dc73c4a1e410f6ba27b6fc6b0e1b
fammad <fred.mmov@gmail.com>
2023-11-25 12:19:37 -0800

    added_2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	if (d->age != NULL)
+		printf("Age: %.f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	if (d->owner != NULL)
+		printf("Owner: %.f\n", d->owner);
+	else
+	printf("Owner: (nil)\n");
+}
