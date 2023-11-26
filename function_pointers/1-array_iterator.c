d36b9cd601f7c6aa3a860f84437500865b5531e0
fammad <fred.mmov@gmail.com>
2023-11-26 09:29:53 -0800

    added_0

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,48 +1,18 @@
-#include <stdio.h>
 #include "function_pointers.h"
+
 /**
- * array_iterator - Executes a function on each element of an array.
- * @array: The array to iterate over.
- * @size: The size of the array.
- * @action: A pointer to the function to be executed on each element.
+ * array_iterator - Itirator function
+ * @array:  An array pointer
+ * @size:	Lenght
+ * @action:  Void pointer to function
  */
-void array_iterator(int *array, size_t size, void (*action)(int)) {
-    if (array == NULL || action == NULL)
+void array_iterator(int *array, size_t size, void (*action)(int x))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
 	{
-		return;
+		for (i = 0; i < size; ++i)
+			action(array[i]);
 	}
-	int i;
-
-    for (i = 0; i < size; i++) {
-        action(array[i]);
-    }
-}
-
-/**
- * print_elem - Prints an integer.
- * @elem: The integer to print.
- */
-void print_elem(int elem) {
-    printf("%d\n", elem);
-}
-
-/**
- * print_elem_hex - Prints an integer in hexadecimal.
- * @elem: The integer to print.
- */
-void print_elem_hex(int elem) {
-    printf("0x%x\n", elem);
-}
-
-/**
- * main - Checks the code.
- * Return: Always 0.
- */
-int main(void) {
-    int array[5] = {0, 98, 402, 1024, 4096};
-
-    array_iterator(array, 5, &print_elem);
-    array_iterator(array, 5, &print_elem_hex);
-
-    return (0);
 }
