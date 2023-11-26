5fa272fffedea40267e916e2ef625fa1c2086b86
fammad <fred.mmov@gmail.com>
2023-11-26 03:59:37 -0800

    added_4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,12 +19,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
+	{
 		free(new_dog);
 		return (NULL);
+	}
 	new_dog->age = age;
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL);
+	{
 		free(new_dog);
 		return (NULL);
+	}
 	return(new_dog);
 }
