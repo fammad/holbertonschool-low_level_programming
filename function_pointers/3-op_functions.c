1ce7ac128a9a29d9414680709d712ab870c28965
fammad <fred.mmov@gmail.com>
2023-11-26 09:57:59 -0800

    added_3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: add between a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: return subtation between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: return multiplication between a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function that doing something
+ * @a: Variable
+ * @b: Variable
+ * Return: Interger
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Function that doing something
+ * @a: Variable
+ * @b: Variable
+ * Return: something
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
