58aa63061c11b981b4e9b73ae965cea7e1f92387
fammad <fred.mmov@gmail.com>
2023-11-26 09:23:54 -0800

    added_0

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int)) {
     if (array == NULL || action == NULL)
         return;
+	size_t i;
 
-
-    for (size_t i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         action(array[i]);
     }
 }
