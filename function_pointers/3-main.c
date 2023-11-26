3d151277e3a0b4ad53dd3944b667a0a178b800b0
fammad <fred.mmov@gmail.com>
2023-11-26 10:06:42 -0800

    added_3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,35 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point of the program.
- * @argc: Number of arguments.
- * @argv: Array of argument strings.
- *
- * Return: 0 on success.
+ * main - entry point
+ * @argc: arguments number
+ * @argv: double pointer to arguments
+ * Return: O for success
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1 = 0, num2 = 0, calc = 0;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    operation = get_op_func(argv[2]);
-
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
-
-    result = operation(num1, num2);
-    printf("%d\n", result);
-
-    return 0;
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	switch (argv[2][0])
+	{
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			calc = (*get_op_func(argv[2]))(num1, num2);
+			printf("%d\n", calc);
+		break;
+		default:
+			printf("Error\n");
+			exit(99);
+	}
+	return (0);
 }
