1ccc50d8c8c3bb7d5775bb00294ec3d90abc716b
fammad <fred.mmov@gmail.com>
2023-11-26 09:19:07 -0800

    added_0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,36 +2,39 @@
 #include "function_pointers.h"
 #include <ctype.h>
 /**
- * print_uppercase - Prints the name in uppercase.
- * @name: The name to be printed in uppercase.
+ * print_uppercase - Prints the name in uppercase
+ * @name: The name to be printed in uppercase
  */
-void print_uppercase(char *name) {
-    while (*name) {
-        putchar(toupper(*name));
-        name++;
-    }
-    putchar('\n');
+void print_uppercase(char *name)
+{
+	while (*name)
+	{
+		putchar(toupper(*name));
+		name++;
+	}
+	putchar('\n');
 }
-
 /**
  * print_lowercase - Prints the name in lowercase.
  * @name: The name to be printed in lowercase.
  */
-void print_lowercase(char *name) {
-    while (*name) {
-        putchar(tolower(*name));
-        name++;
-    }
-    putchar('\n');
+void print_lowercase(char *name)
+{
+	while (*name)
+	{
+		putchar(tolower(*name));
+		name++;
+	}
+	putchar('\n');
 }
-
 /**
 *print_name - Calls the specified printing function with the provided name.
  * @name: The name to be printed.
  * @f: Function pointer to the printing function.
  */
-void print_name(char *name, void (*f)(char *)) {
-  if (f == NULL)
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
 	return;
-  f(name);
+	f(name);
 }
