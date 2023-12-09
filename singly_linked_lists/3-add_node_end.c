e56c033fa73d4c21b2b52e2f3da7286c11e91373
fammad <fred.mmov@gmail.com>
2023-12-09 12:11:04 -0800

    ADDED 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ *add_node - Function that adds a new node
+ *@head: Working list
+ *@str: Added string
+ *Return: new Node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(new_node->str);
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
+	else
+		list_t *current = *head;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
+	return (new_node);
+}
