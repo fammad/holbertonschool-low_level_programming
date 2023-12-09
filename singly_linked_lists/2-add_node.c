62ef06f534f0e0e76f772507c5d99092490f6883
fammad <fred.mmov@gmail.com>
2023-12-09 11:19:22 -0800

    Updated 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,9 +8,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	if (str == NULL)
 		return (NULL);
-	list_t *new_node;
-
-	new_node = malloc(sizeof(list_t));
+	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
