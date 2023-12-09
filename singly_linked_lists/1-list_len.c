3071726dc6f6d9753a9b1ce0bbc0ace96a1ce616
fammad <fred.mmov@gmail.com>
2023-12-09 10:00:20 -0800

    ADDED 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+/**
+ *count_number - Function that counts number of nodes in h
+ *@h: Working list
+ *Return: Number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count_number = 0;
+
+	while(h != null)
+	{
+		h = h->next;
+		count_number++;
+	}
+	return (count_number);
+}
