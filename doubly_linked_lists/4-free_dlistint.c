fda29179c37c47662aa4a909067eaa0fd5910dcd
fammad <fred.mmov@gmail.com>
2023-12-10 11:42:53 -0800

    ADDED 5

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - check the code
+ * @head: var
+ * Return: Always 0.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
