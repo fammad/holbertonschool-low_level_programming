c178be3bd93dd89f22d385e9709a99e7d1acdd6f
fammad <fred.mmov@gmail.com>
2023-12-10 09:22:33 -0800

    ADDED

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+/**
+ *
+ *
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, digit;
+	unsigned int result;
+
+	if (b == NULL);
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		digit = b[i] - '0';
+		result <<= 1;
+		result += digit;
+	}
+	return (result);
+}
