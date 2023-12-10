fab8030c5909a384119f58eeba1ef68a6a05a87d
fammad <fred.mmov@gmail.com>
2023-12-10 10:42:00 -0800

    ADDED_0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list
+ *
+ * @h: Contant List
+ *
+ * Return: size_t
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
