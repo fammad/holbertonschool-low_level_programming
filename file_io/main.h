9bc8fe5bf31aed265b1e08e621ba4b76daf54c8d
fammad <fred.mmov@gmail.com>
2023-12-16 13:08:28 -0800

    ADDED 0

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <sys.h>
+#include <stdio.h>
+#incldue <stdlib.h>
 #include <unistd.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
