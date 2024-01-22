ebbd6eaefd260f78fed3802559b363e36d45bbce
fammad <fred.mmov@gmail.com>
2024-01-21 23:13:13 -0800

    Fixed

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
 #ifndef SEARCH_HEADER
 #define SEARCH_HEADER
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+
 #include <stdio.h>
 #include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
