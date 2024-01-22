4762c1e0816ecfab8c023b37ebe1ea8750cd89e1
fammad <fred.mmov@gmail.com>
2024-01-21 23:11:17 -0800

    ADDED SEARCH ALGOS

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -2,4 +2,6 @@
 #define SEARCH_HEADER
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+#include <stdio.h>
+#include <stdlib.h>
 #endif
